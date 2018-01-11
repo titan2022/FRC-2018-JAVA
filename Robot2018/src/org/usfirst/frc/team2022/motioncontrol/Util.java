891a5df17b52f85d232a2f0689bf89eb5807d227
trajing <trajingplaysgames@gmail.com>
2018-01-11 15:37:32 -0600

    Created velocity and position methods in rotations

--- Robot2018/src/org/usfirst/frc/team2022/motioncontrol/Util.java
+++ Robot2018/src/org/usfirst/frc/team2022/motioncontrol/Util.java
@@ -1,5 +1,19 @@
 package org.usfirst.frc.team2022.motioncontrol;
 
 public class Util {
-
+	// position in rotations
+	private static double rotPos(double maxV, double distance, double time) {
+		return (1024 * time*time*time * maxV*maxV*maxV * (64 * time*time * maxV*maxV
+				                                        - 300 * distance * time * maxV
+				                                        + 375 * distance*distance))/
+				(253125 * distance*distance*distance*distance);
+	}
+	
+	// velocity in rotations
+	private static double rotVel(double maxV, double distance, double time) {
+		return (5120 * time*time * maxV*maxV*maxV * (64 * time*time * maxV*maxV
+				                             - 240*distance*time*maxV
+				                             + 225 * distance*distance))/
+				(253125 * distance*distance*distance*distance);
+	}
 }
