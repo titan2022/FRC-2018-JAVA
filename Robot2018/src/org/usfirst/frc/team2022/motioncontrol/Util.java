bd161e36ffe155ef963a4fbf179a06d37c90ac9c
trajing <trajingplaysgames@gmail.com>
2018-01-12 09:01:02 -0600

    Added converted position and velocity functions

--- Robot2018/src/org/usfirst/frc/team2022/motioncontrol/Util.java
+++ Robot2018/src/org/usfirst/frc/team2022/motioncontrol/Util.java
@@ -4,7 +4,16 @@ public class Util {
 	// feet * conversionFactor = rotations
 	static final float conversionFactor = 60;
 	
-	// position in rotations
+	
+	public static double position(double maxV, double distance, double time) {
+		return rotPos(maxV * conversionFactor, distance * conversionFactor, time);
+	}
+	
+	public static double velocity(double maxV, double distance, double time) {
+		return rotVel(maxV * conversionFactor, distance * conversionFactor, time);
+	}
+	
+	// position function, all args in rotations
 	private static double rotPos(double maxV, double distance, double time) {
 		return (1024 * time*time*time * maxV*maxV*maxV * (64 * time*time * maxV*maxV
 				                                        - 300 * distance * time * maxV
@@ -12,7 +21,7 @@ public class Util {
 				(253125 * distance*distance*distance*distance);
 	}
 	
-	// velocity in rotations
+	// velocity function, all args in rotations
 	private static double rotVel(double maxV, double distance, double time) {
 		return (5120 * time*time * maxV*maxV*maxV * (64 * time*time * maxV*maxV
 				                             - 240*distance*time*maxV
