9a9d5b29b2b18183ab77758092822bfa9274722d
trajing <trajingplaysgames@gmail.com>
2018-01-30 19:39:08 -0600

    Revert "Merge remote-tracking branch 'origin/DriveBase' into motion-control"
    Apparently I wasn't supposed to do this.

--- Robot2018/src/org/usfirst/frc/team2022/robot/Robot.java
+++ Robot2018/src/org/usfirst/frc/team2022/robot/Robot.java
@@ -1,17 +1,15 @@
+
 package org.usfirst.frc.team2022.robot;
 
-import org.usfirst.frc.team2022.robot.commands.DriveCommand;
-import org.usfirst.frc.team2022.robot.subsystems.DriveSubsystem;
-
-import edu.wpi.cscore.UsbCamera;
-import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
+import org.usfirst.frc.team2022.robot.commands.DriveCommand;
+import org.usfirst.frc.team2022.robot.subsystems.DriveSubsystem;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -21,213 +19,98 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * directory.
  */
 public class Robot extends IterativeRobot {
-	
 
-
-	//Instantiate Subsystems
-	public static final DriveSubsystem driveSubsystem = new DriveSubsystem();
-	//LIGHTS!!
-//	public static final LightSubsystem lights = new LightSubsystem();
-	
-	//Create References to commands
-	public DriveCommand driveCommand;
-//	public LightCommand lightCommand;
-	//Autonomous
-	CommandGroup autonomousCommand = new CommandGroup();
-	SendableChooser<String> autoTypeChooser;
-	SendableChooser<String> autoShooterChooser;
-	SendableChooser<String> autoGearChooser;
-	double position;
-	double gear;
-	
-	
-	//Create reference to OI
+	public static final DriveSubsystem exampleSubsystem = new DriveSubsystem();
 	public static OI oi;
-	public XboxMap xboxMap = new XboxMap();
-	
-	
-	//Initialization code ran when you turn on the robot
 
-    public void robotInit() {    	
+	Command autonomousCommand;
+	SendableChooser<Command> chooser = new SendableChooser<>();
 
-    	//Instantiate OI
-    	oi = new OI();
-    	
-    	//Instantiate Commands
-    	driveCommand = new DriveCommand();
-//    	lightCommand = new LightCommand(0);
-    	UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
-    	camera.setResolution(480, 360);
-    	camera.setFPS(30);
-    	
-    	//Create thread for streaming cameras
-//    	Thread t = new Thread(new Runnable(){
-//    		public void run(){
-//    			boolean allowCam1 = false;
-//        		
-//        		UsbCamera camera1 = CameraServer.getInstance().startAutomaticCapture(0);
-//                camera1.setResolution(640, 480);
-//                camera1.setFPS(15);
-//                UsbCamera camera2 = CameraServer.getInstance().startAutomaticCapture(1);
-//                camera2.setResolution(640, 480);
-//                camera2.setFPS(15);
-//                
-//                CvSink cvSink1 = CameraServer.getInstance().getVideo(camera1);
-//                CvSink cvSink2 = CameraServer.getInstance().getVideo(camera2);
-//                CvSource outputStream = CameraServer.getInstance().putVideo("Switcher", 640, 480);
-//                
-//                Mat image = new Mat();
-//                
-//                while(!Thread.interrupted()) {
-//                	
-//                	if(oi.xbox.GetStartValue()) {
-//                		allowCam1 = !allowCam1;
-//                	}
-//                	
-//                    if(allowCam1){
-//                      cvSink2.setEnabled(false);
-//                      cvSink1.setEnabled(true);
-//                      cvSink1.grabFrame(image);
-//                    } else{
-//                      cvSink1.setEnabled(false);
-//                      cvSink2.setEnabled(true);
-//                      cvSink2.grabFrame(image);     
-//                    }
-//                    
-//                    outputStream.putFrame(image);
-//                }
-//                
-//    		}
-//    	});
-//    	t.start();
+	/**
+	 * This function is run when the robot is first started up and should be
+	 * used for any initialization code.
+	 */
+	@Override
+	public void robotInit() {
+		oi = new OI();
+		chooser.addDefault("Default Auto", new DriveCommand());
+		// chooser.addObject("My Auto", new MyAutoCommand());
+		SmartDashboard.putData("Auto mode", chooser);
+	}
 
-		
-    	autoTypeChooser = new SendableChooser<String>();
-    	autoTypeChooser.addDefault("Position Gear 2 (Middle)", "gearOption2"); 
-    	autoTypeChooser.addObject("Position Gear 1 (Right)", "gearOption1"); 
-    	autoTypeChooser.addObject("Position Gear 3 (Left)", "gearOption3");
-    	autoTypeChooser.addObject("Shooter (Blue Side)", "blue");
-    	autoTypeChooser.addObject("Shooter (Red Side)", "red");
-    	autoTypeChooser.addObject("Vision Shooter", "vision");
-
-//    	autoTypeChooser.addObject("Left starting position", "shooterOption1"); 
-//    	autoTypeChooser.addObject("Center starting position", "shooterOption2"); 
-//    	autoTypeChooser.addObject("Right starting position", "shooterOption3"); 
-    	SmartDashboard.putData("Autonomous Mode", autoTypeChooser);
-//    	
-//    	autoGearChooser = new SendableChooser<String>();
-//    	autoGearChooser.addDefault("Position Gear 1 (Right)", "gearOption1"); 
-//    	autoGearChooser.addObject("Position Gear 2 (Middle)", "gearOption2"); 
-//    	autoGearChooser.addObject("Position Gear 3 (Left)", "gearOption3"); 
-//    	SmartDashboard.putData("Auto Gear Positions", autoGearChooser);
-//    	
-//    	autoShooterChooser = new SendableChooser<String>();
-//    	autoShooterChooser.addDefault("Left starting position", "shooterOption1"); 
-//    	autoShooterChooser.addObject("Center starting position", "shooterOption2"); 
-//    	autoShooterChooser.addObject("Right starting position", "shooterOption3"); 
-//    	SmartDashboard.putData("Auto Field Position", autoShooterChooser);  	
-    	
-    }
-    
-    
-    //This starts the methods for autonomous
-    public void autonomousInit() {
-    	try{
-    		if(autoTypeChooser.getSelected().equals("gearOption1")){
-        		autonomousCommand = new AutoGearCommandGroup(1);
-        	}
-        	else if(autoTypeChooser.getSelected().equals("gearOption2")){
-        		autonomousCommand = new AutoGearCommandGroup(2);
-        	}
-        	else if(autoTypeChooser.getSelected().equals("gearOption3")){
-        		autonomousCommand = new AutoGearCommandGroup(3);
-        	}
-        	else if(autoTypeChooser.getSelected().equals("blue")){
-        		autonomousCommand = new AutoNewShooterCommandGroup("Blue");
-        	}
-        	else if(autoTypeChooser.getSelected().equals("red")){
-        		autonomousCommand = new AutoNewShooterCommandGroup("Red");
-        	}
-        	else if(autoTypeChooser.getSelected().equals("vision")){
-        		autonomousCommand = new AutoNewShooterVisionCommandGroup();
-        	}
-//        	else if(autoTypeChooser.getSelected().equals("shooterOption1")){
-//        		autonomousCommand = new AutoShooterLeftCommandGroup();
-//        	}
-//        	else if(autoTypeChooser.getSelected().equals("shooterOption2")){
-//        		autonomousCommand = new AutoShooterCenterCommandGroup();
-//        	}
-//        	else if(autoTypeChooser.getSelected().equals("shooterOption3")){
-//        		autonomousCommand = new AutoShooterRightCommandGroup();
-//        	}
-    	}
-    	catch(Exception ex){
-    		System.out.println(ex);
-    	}
-    	ultrasonicCommand.start();
-    	
-    	autonomousCommand.start();
-    	
-//    	else if(autoTypeChooser.getSelected().equals("Shooter")){
-//    		if(autoShooterChooser.getSelected().equals("shooterOption1")){
-//    			autonomousCommand = new AutoShooterLeftCommandGroup();
-//    		}
-//    		else if(autoShooterChooser.getSelected().equals("shooterOption2")){
-//    			autonomousCommand = new AutoShooterCenterCommandGroup();
-//    		}
-//    		else if(autoShooterChooser.getSelected().equals("shooterOption3")){
-//    			autonomousCommand = new AutoShooterRightCommandGroup();
-//    		}
-//    	}
-//    	else if(autoTypeChooser.getSelected().equals("Straight")){
-//    		autonomousCommand = new CommandGroup();
-//    	}
-    }
-    
-    //This starts the methods for teleop and stops methods for autonomous
-    @Override
-	public void teleopInit() {
-    	driveCommand.start();
-//    	lightCommand.start();
-    }
-    
-    //This stops the methods for autonomous
+	/**
+	 * This function is called once each time the robot enters Disabled mode.
+	 * You can use it to reset any subsystem information you want to clear when
+	 * the robot is disabled.
+	 */
 	@Override
 	public void disabledInit() {
-		driveCommand.cancel();
-	}
-    
-	//Methods below this line do not need to be edited/////////////////////////////////////////////////////////////////////////
-    /**
-     * This function is called periodically during autonomous
-     */
-    public void autonomousPeriodic() {
-    	Scheduler.getInstance().run();
-    }
 
-    @Override
-	public void robotPeriodic() {
-		// TODO Auto-generated method stub
-    	Scheduler.getInstance().run();
 	}
-    
-    /**
-     * This function is called periodically during operator control
-     */
-    public void teleopPeriodic() {    	
-    	Scheduler.getInstance().run();
-    }
-    
-    /**
-     * This function is called periodically during test mode
-     */
-    public void testPeriodic() {
-    	LiveWindow.run();
-    }
-
 
 	@Override
 	public void disabledPeriodic() {
 		Scheduler.getInstance().run();
+	}
+
+	/**
+	 * This autonomous (along with the chooser code above) shows how to select
+	 * between different autonomous modes using the dashboard. The sendable
+	 * chooser code works with the Java SmartDashboard. If you prefer the
+	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
+	 * getString code to get the auto name from the text box below the Gyro
+	 *
+	 * You can add additional auto modes by adding additional commands to the
+	 * chooser code above (like the commented example) or additional comparisons
+	 * to the switch structure below with additional strings & commands.
+	 */
+	@Override
+	public void autonomousInit() {
+		autonomousCommand = chooser.getSelected();
+
+		/*
+		 * String autoSelected = SmartDashboard.getString("Auto Selector",
+		 * "Default"); switch(autoSelected) { case "My Auto": autonomousCommand
+		 * = new MyAutoCommand(); break; case "Default Auto": default:
+		 * autonomousCommand = new ExampleCommand(); break; }
+		 */
+
+		// schedule the autonomous command (example)
+		if (autonomousCommand != null)
+			autonomousCommand.start();
+	}
+
+	/**
+	 * This function is called periodically during autonomous
+	 */
+	@Override
+	public void autonomousPeriodic() {
+		Scheduler.getInstance().run();
+	}
+
+	@Override
+	public void teleopInit() {
+		// This makes sure that the autonomous stops running when
+		// teleop starts running. If you want the autonomous to
+		// continue until interrupted by another command, remove
+		// this line or comment it out.
+		if (autonomousCommand != null)
+			autonomousCommand.cancel();
+	}
+
+	/**
+	 * This function is called periodically during operator control
+	 */
+	@Override
+	public void teleopPeriodic() {
+		Scheduler.getInstance().run();
+	}
+
+	/**
+	 * This function is called periodically during test mode
+	 */
+	@Override
+	public void testPeriodic() {
+		LiveWindow.run();
 	}
 }
